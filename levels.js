3abf09ea732c85d24cecf80c5561493e6cb7c514
tclownerig <kyliemommy676@gmail.com>
2026-04-27 18:46:18 +0200

    Create levels.js

---
+++ levels.js
@@ -0,0 +1,16 @@
+const levels = [
+  {
+    placement: 1,
+    name: "Stairs Of Despair",
+    creator: "Boingmd",
+    tps: 32767,
+    verifier: "Boingmd",
+    fps: "480",
+    video: "MdgwQK6Zf0w",
+    levelId: "Unknown",
+    points: "250.00",
+    records: []
+  }
+];
+
+levels.sort((a, b) => a.placement - b.placement);
